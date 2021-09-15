5fea7043ae7ac58d4e5046f5f8957bc0546e84ff
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-09-15 12:15:47 +0200

    Setup for css and php
    php:
    - added templates
       - header template
    
    css:
    - added variables
       - color
       - font sizes
       - shadows
    - added font
       - open sans
    - other setup stuff

---
+++ php/index.php
@@ -0,0 +1,28 @@
+<?php
+include 'template.php';
+
+?>
+
+<?= template_head("Home") ?>
+
+<body>
+
+<?= template_header("Home") ?>
+
+<main>
+    <h1>test</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, fuga in inventore maiores molestias
+        reiciendis repudiandae veritatis vitae voluptatibus voluptatum. A aliquam animi aspernatur cum deserunt dicta,
+        distinctio eligendi facere inventore libero modi nisi odit possimus quam quidem recusandae, unde! Eligendi
+        incidunt laboriosam mollitia necessitatibus numquam, ut. Cum, facilis, suscipit.</p>
+    <h2>test 2</h2>
+    <ul>
+        <li><a href="#">fietsen</a></li>
+        <li><a href="#">pannenkoeken</a></li>
+        <li><a href="#">Belasting fraude voor dummies</a></li>
+        <li><a href="#">Atlassen</a></li>
+    </ul>
+
+</main>
+</body>
+</html>
