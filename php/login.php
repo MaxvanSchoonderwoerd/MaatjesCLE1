715dcb102869935df8a2c2c04848c0092ac808f5
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-09-27 14:41:10 +0200

    added multiple pages

---
+++ php/login.php
@@ -0,0 +1,16 @@
+<?php
+include 'template.php';
+
+?>
+
+<?= template_head("Login") ?>
+
+<body>
+
+<?= template_header("Login") ?>
+
+<main>
+
+</main>
+</body>
+</html>
---
+++ php/logout.php
@@ -0,0 +1,16 @@
+<?php
+include 'template.php';
+
+?>
+
+<?= template_head("Logout") ?>
+
+<body>
+
+<?= template_header("Logout") ?>
+
+<main>
+
+</main>
+</body>
+</html>
