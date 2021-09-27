ee5cb3ffbf951dc331fe3cf8c9ad14b541e288d0
Pim Brederode <pimbrederode@live.com>
2021-09-27 14:34:45 +0200

    tetetetetettetettete

--- php/index.php
+++ php/index.php
@@ -6,15 +6,6 @@ include 'template.php';
 <?= template_head("Home") ?>
 
 <body>
-<header>
-    <div class="header">
-        <a href="#" class="listItem"><img class="logo listItem" src="https://i.pravatar.cc/125/" alt="Maatjes"></a>
-        <a href="#" class="listItem">Upload</a>
-        <a href="#" class="listItem">Profiel</a>
-        <a href="#" class="listItem">Login</a>
-        <a href="#" class="listItem">Uitloggen</a>
-    </div>
-</header>
 
 <?= template_header("Home") ?>
 
