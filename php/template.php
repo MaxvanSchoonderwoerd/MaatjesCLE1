0172ad688fccd7eee6a0a8e17145e8742db7287b
Pim Brederode <pimbrederode@live.com>
2021-09-27 14:11:18 +0200

    Unfinished header added

--- php/template.php
+++ php/template.php
@@ -17,16 +17,5 @@ EOT;
 
 function template_header($page) {
 //    header goes here
-    echo <<<EOT
-<!doctype html>
-<html lang="en">
-<body>
-    <header>
-        <div class="header">
-            <h1>Maatjes</h1>
-            <h2>Login</h2>
-        </div>
-    </header>
-</body>
-EOT;
+
 }
