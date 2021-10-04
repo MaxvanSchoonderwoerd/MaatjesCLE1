30fe54c378c33da2ad5b81f1a52b42aee360a1cb
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-10-04 10:58:24 +0200

    added text area

--- php/upload.php
+++ php/upload.php
@@ -7,29 +7,29 @@ include 'template.php';
 
 <body>
 
-    <?= template_header("Upload") ?>
+<?= template_header("Upload") ?>
 
-    <main>
+<main>
     <section class="infoSection">
-            <div class="infoContainer">
-                <h1 class="infoTitle">Plaats jou klus hier!!</h1>
-                <p class="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident,
-                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-        </section>
-        <section class="loginSection">
-            <form action="" class="loginContainer loginContainerMargin">
+        <div class="infoContainer">
+            <h1 class="infoTitle">Plaats jou klus hier!!</h1>
+            <p class="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </section>
+    <section class="loginSection">
+        <form action="" class="loginContainer loginContainerMargin">
             <h1>Plaats een klus</h1>
             <input class="input loginInput" type="text" id="fname" name="fname" placeholder="Titel">
-            <input class="input loginInput overview" type="text" id="fname" name="fname" placeholder="Omschrijving">
-            <input class="input loginInput" type="text" id="fname" name="fname" value="plaats">
+            <textarea class="input loginInput overview" type="text" id="fname" name="fname" placeholder="Omschrijving"></textarea>
+            <input class="input loginInput" type="text" id="fname" name="fname" placeholder="plaats">
             <input class="input loginButton" type="submit" value="Plaats jou klus">
-            </form>
-        </section>
-    </main>
+        </form>
+    </section>
+</main>
 </body>
