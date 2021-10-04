c01927d67b5f36d003c5dc11de088f52cc2aad51
Pim Brederode <pimbrederode@live.com>
2021-10-04 10:18:15 +0200

    Upload page updated

--- php/upload.php
+++ php/upload.php
@@ -10,11 +10,26 @@ include 'template.php';
     <?= template_header("Upload") ?>
 
     <main>
-        <section class="jobInfo">
+    <section class="infoSection">
+            <div class="infoContainer">
+                <h1 class="infoTitle">Place your job here!</h1>
+                <p class="infoBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident,
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </section>
+        <section class="loginSection">
+            <form action="" class="loginContainer loginContainerMargin">
             <h1>Place a job</h1>
-            <input class="jobTitle" type="text" id="fname" name="fname" value="Title">
-            <input class="jobInputInfo" type="text" id="fname" name="fname" value="Overview">
-            <input class="jobPlace" type="text" id="fname" name="fname" value="Place">
+            <input class="input loginInput" type="text" id="fname" name="fname" placeholder="Title">
+            <input class="input loginInput" type="text" id="fname" name="fname" placeholder="Overview">
+            <input class="input loginInput" type="text" id="fname" name="fname" value="Place">
+            <input class="input loginButton" type="submit" value="Place your job">
+            </form>
         </section>
     </main>
 </body>
