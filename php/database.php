79d0678650024b988df5bba06efc52b44e2e14c0
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-10-04 13:54:59 +0200

    added php for loading in jobs

--- php/database.php
+++ php/database.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $name = "root";
 $password = "";
-$database = "ReserveeringSysteem";
+$database = "Maatjes";
 
 $db = mysqli_connect($host, $name, $password, $database) or die ("Error: " . mysqli_connect_error());
 
